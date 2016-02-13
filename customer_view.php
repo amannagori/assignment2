f4ee4eb3e145dd8a008ee552a256d6cb258945fd
amannagori <amannagori07@gmail.com>
2016-02-12 20:53:14 -0600

    Code modified

--- customer_view.php
+++ customer_view.php
@@ -17,60 +17,93 @@
 	<body>
 
 		<div class="container-fluid">
-			<div class="table-responsive">
-			  <table class="table table-striped">
-			    <tr>
-			    	<th>First Name</th>
-			    	<th>Last Name</th>
-			    	<th>Address</th>
-			    	<th>City</th>
-			    	<th>State</th>
-			    	<th>Zip</th>
-			    	<th>Email</th>
-			    	<th>Phone</th>
-			    </tr>
-
-			    <?php // query.php
-
-						// require_once 'login.php';
-
-						// login.php
-						$hn = 'www.it354.com';
-						$db = 'it354_students';
-						$un = 'it354_students';
-						$pw = 'steinway';
-
-						$conn = new mysqli($hn, $un, $pw, $db);
-						if ($conn->connect_error) die($conn->connect_error);
-
-						$query = "SELECT * FROM customers";
-						$result = $conn->query($query);
-						if (!$result) die($conn->error);
-
-						$rows = $result->num_rows;
-
-						for ($j = 0 ; $j < $rows ; ++$j)
-						{
-						  $result-> data_seek($j);
-						  $row = $result->fetch_array(MYSQLI_ASSOC);
-
-						  echo '<tr><td>' . $row['firstName'] . '</td>';
-						  echo '<td>' . $row['lastName'] . '</td>';
-						  echo '<td>' . $row['address'] . '</td>';
-						  echo '<td>' . $row['city'] . '</td>';
-						  echo '<td>' . $row['state'] . '</td>';
-						  echo '<td>' . $row['zip'] . '</td>';
-						  echo '<td>' . $row['email'] . '</td>';
-						  echo '<td>' . $row['phone'] . '</td></tr>';
-						}
-
-						$result->close();
-						$conn->close();
-				?>
+			<div class="navbar navbar-fixed-top">
+				<div class="container">
+					
+					<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+					<button class="navbar-toggle" data-target=".navbar-responsive-collapse" data-toggle="collapse" type="button">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				
+					<a class="navbar-brand" href="#">Assignment2</a>
+					
+					<div class="nav-collapse collapse navbar-responsive-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active">
+								<a href="#">View</a>
+							</li>
+							<li>
+								<a href="customer_add.html">Add</a>
+							</li>
+						</ul>
+					</div>											
+				</div><!-- end container -->
+			</div><!-- end navbar -->
 
 
-			  </table>
-			</div>
+
+
+			<div class="row" style="margin-top:80px">
+				<div class="col-sm-8">
+					<h2>Add a Customer</h2>
+						<div class="table-responsive">
+						  <table class="table table-striped">
+						    <tr>
+						    	<th>First Name</th>
+						    	<th>Last Name</th>
+						    	<th>Address</th>
+						    	<th>City</th>
+						    	<th>State</th>
+						    	<th>Zip</th>
+						    	<th>Email</th>
+						    	<th>Phone</th>
+						    </tr>
+
+						    <?php // query.php
+
+									// require_once 'login.php';
+
+									// login.php
+									$hn = 'www.it354.com';
+									$db = 'it354_students';
+									$un = 'it354_students';
+									$pw = 'steinway';
+
+									$conn = new mysqli($hn, $un, $pw, $db);
+									if ($conn->connect_error) die($conn->connect_error);
+
+									$query = "SELECT * FROM customers";
+									$result = $conn->query($query);
+									if (!$result) die($conn->error);
+
+									$rows = $result->num_rows;
+
+									for ($j = 0 ; $j < $rows ; ++$j)
+									{
+									  $result-> data_seek($j);
+									  $row = $result->fetch_array(MYSQLI_ASSOC);
+
+									  echo '<tr><td>' . $row['firstName'] . '</td>';
+									  echo '<td>' . $row['lastName'] . '</td>';
+									  echo '<td>' . $row['address'] . '</td>';
+									  echo '<td>' . $row['city'] . '</td>';
+									  echo '<td>' . $row['state'] . '</td>';
+									  echo '<td>' . $row['zip'] . '</td>';
+									  echo '<td>' . $row['email'] . '</td>';
+									  echo '<td>' . $row['phone'] . '</td></tr>';
+									}
+
+									$result->close();
+									$conn->close();
+							?>
+
+
+						  </table>
+						</div> <!--
+				</div>
+			</div>	
 
 		</div>
 
