ed53d130910d82d913de823eb424505de44ce1cf
amannagori <amannagori07@gmail.com>
2016-02-12 21:24:41 -0600

    Added Container class to bootstrap table

--- customer_view.php
+++ customer_view.php
@@ -44,67 +44,68 @@
 
 
 
+			<div class="container">
+				<div class="row" style="margin-top:80px">
+					<div class="col-sm-12">
+						<h2>Add a Customer</h2>
+						<!-- create a bootstrap table -->
+							<div class="table-responsive">
+							  <table class="table table-striped">
+							    <tr>
+							    	<th>First Name</th>
+							    	<th>Last Name</th>
+							    	<th>Address</th>
+							    	<th>City</th>
+							    	<th>State</th>
+							    	<th>Zip</th>
+							    	<th>Email</th>
+							    	<th>Phone</th>
+							    </tr>
 
-			<div class="row" style="margin-top:80px">
-				<div class="col-sm-10">
-					<h2>Add a Customer</h2>
-					<!-- create a bootstrap table -->
-						<div class="table-responsive">
-						  <table class="table table-striped">
-						    <tr>
-						    	<th>First Name</th>
-						    	<th>Last Name</th>
-						    	<th>Address</th>
-						    	<th>City</th>
-						    	<th>State</th>
-						    	<th>Zip</th>
-						    	<th>Email</th>
-						    	<th>Phone</th>
-						    </tr>
+							    <?php // query.php
 
-						    <?php // query.php
+										// require_once 'login.php';
 
-									// require_once 'login.php';
+										// login.php
+										$hn = 'www.it354.com';
+										$db = 'it354_students';
+										$un = 'it354_students';
+										$pw = 'steinway';
 
-									// login.php
-									$hn = 'www.it354.com';
-									$db = 'it354_students';
-									$un = 'it354_students';
-									$pw = 'steinway';
+										$conn = new mysqli($hn, $un, $pw, $db);
+										if ($conn->connect_error) die($conn->connect_error);
 
-									$conn = new mysqli($hn, $un, $pw, $db);
-									if ($conn->connect_error) die($conn->connect_error);
+										$query = "SELECT * FROM customers";
+										$result = $conn->query($query);
+										if (!$result) die($conn->error);
 
-									$query = "SELECT * FROM customers";
-									$result = $conn->query($query);
-									if (!$result) die($conn->error);
+										$rows = $result->num_rows;
 
-									$rows = $result->num_rows;
+										for ($j = 0 ; $j < $rows ; ++$j)
+										{
+										  $result-> data_seek($j);
+										  $row = $result->fetch_array(MYSQLI_ASSOC);
 
-									for ($j = 0 ; $j < $rows ; ++$j)
-									{
-									  $result-> data_seek($j);
-									  $row = $result->fetch_array(MYSQLI_ASSOC);
+										  echo '<tr><td>' . $row['firstName'] . '</td>';
+										  echo '<td>' . $row['lastName'] . '</td>';
+										  echo '<td>' . $row['address'] . '</td>';
+										  echo '<td>' . $row['city'] . '</td>';
+										  echo '<td>' . $row['state'] . '</td>';
+										  echo '<td>' . $row['zip'] . '</td>';
+										  echo '<td>' . $row['email'] . '</td>';
+										  echo '<td>' . $row['phone'] . '</td></tr>';
+										}
 
-									  echo '<tr><td>' . $row['firstName'] . '</td>';
-									  echo '<td>' . $row['lastName'] . '</td>';
-									  echo '<td>' . $row['address'] . '</td>';
-									  echo '<td>' . $row['city'] . '</td>';
-									  echo '<td>' . $row['state'] . '</td>';
-									  echo '<td>' . $row['zip'] . '</td>';
-									  echo '<td>' . $row['email'] . '</td>';
-									  echo '<td>' . $row['phone'] . '</td></tr>';
-									}
-
-									$result->close();
-									$conn->close();
-							?>
+										$result->close();
+										$conn->close();
+								?>
 
 
-						 </table><!-- end table -->
-					</div><!-- end table-responsive -->
-				</div> <!-- End col -->
-			</div>	<!-- end row -->
+							 </table><!-- end table -->
+						</div><!-- end table-responsive -->
+					</div> <!-- End col -->
+				</div>	<!-- end row -->
+			</div> <!-- End of Container -->
 
 		</div><!-- end container-fluid -->
 
