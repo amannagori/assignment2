9b663ea39d3643a3ec58bdf519f366ee0a848b95
amannagori <amannagori07@gmail.com>
2016-02-13 19:40:07 -0600

    Added JS links at the bottom of Customer_View PHP file

--- customer_view.php
+++ customer_view.php
@@ -109,8 +109,19 @@
 
 		</div><!-- end container-fluid -->
 
+	<!-- All Javascript at the bottom of the page for faster page loading -->
+		
+	<!-- First try for the online version of jQuery-->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	
+	<!-- If no online access, fallback to our hardcoded version of jQuery -->
+	<script>window.jQuery || document.write('<script src="dist/js/jquery-1.8.2.min.js"><\/script>')</script>
+	
 	<!-- Bootstrap JS -->
 	<script src="dist/js/bootstrap.min.js"></script>
+	
+	<!-- Custom JS -->
+	<script src="dist/js/script.js"></script>
 	
 	</body>
 </html>
